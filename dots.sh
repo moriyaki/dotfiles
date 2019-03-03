3d08a1c0134d57f0067a3aefa18a43dd6182f577
moriyaki <moriyaki999@gmail.com>
2019-03-04 02:00:26 +0900

    WSL detect

--- dots.sh
+++ dots.sh
@@ -4,19 +4,30 @@ set -e
 DOT_DIRECTORY="${HOME}/dotfiles"
 cd ${DOT_DIRECTORY}
 
+# Detect OS test.
+case "$(expr substr $(uname -s) 1 5)" in
+  "Linux" )
+    if [ -f /proc/sys/fs/binfmt_misc/WSLInterop ]; then
+      OS=WSL
+    else
+      OS=Linux
+    fi
+    ;;
+  "MINGW" ) OS=MINGW   ;;
+  "Darwi" ) OS=Mac     ;; # Not tested.
+  *)
+    echo "Unknown OS"
+    exit 1
+    ;;
+esac
+
+echo ${OS}
+
 # Restore the replaced from '.' to '_' filenames.
 for v in _*
 do
   ln -snfv ${DOT_DIRECTORY}/${v} ${HOME}/$(echo ${v} | sed "s/^\_/\./")
 done
-
-# Detect OS test.
-case "$(expr substr $(uname -s) 1 5)" in
-  "Linux" ) OS=Linux   ;;
-  "MINGW" ) OS=MINGW   ;;
-  "Darwi" ) OS=Mac     ;; # Not tested.
-  *)        OS=Unknown ;;
-esac
 
 for f in *.*
 do
