48362ee4bf24c634dce79084ec856558af7e1dc4
moriyaki <moriyaki999@gmail.com>
2020-01-28 21:51:45 +0900

    dots.shとプロンプト調整

--- dots.sh
+++ dots.sh
@@ -47,6 +47,6 @@ if [[ ${OS} == "MINGW" ]]; then
 fi
 
 curl -o ~/.git-completion.bash https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
-curl -o ~/.git-prompt.bash https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
+curl -o ~/.git-prompt.sh https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
 chmod a+x ~/.git-completion.bash
 chmod a+x ~/.git-prompt.sh
